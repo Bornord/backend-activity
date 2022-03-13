037dae648eea57ef088d2a1fe3b540bac9905ede
Bornord <best.enguerran@gmail.com>
2022-03-13 12:47:28 +0100

    1er jet

--- README.md
+++ README.md
@@ -9,3 +9,22 @@ npm install _
 => À CHAQUE FOIS et cela inclut :
     - Démarrage serveur_frontend
     - Import package express
+
+# R2 : construction d'un json avec plusieurs éléments
+Bon exemple :
+    // création de l'objet
+    const products = [
+        {
+            name: "p1",
+            description: 'd1',
+            price: 10,
+            inStock: true,
+        },{
+            name: 'p2',
+            description: 'd2',
+            price: 20,
+            inStock: false,
+        }
+    ];
+    // retour du json pour le serveur qui a fait la requête. 
+    res.status(300).json(products);
--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 //package imports
 const express = require('express');
 
-// config
+// config serveur
 const app = express();
 app.use(express.json());
 
+// config des autorisations
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -12,12 +13,39 @@ app.use((req, res, next) => {
     next();
   });
 
+// instruction pour debug
 app.use((req,res,next) => {
     console.log('instruction reçue');
+    /*
     res.status(200).json({
         msg: 'bien reçu, le traitement va commencer !'
     });
+    */
     next();
 });
+
+
+app.get('/api/products',(req,res,next) => {
+    const products = [
+        {
+            name: "p1",
+            description: 'd1',
+            price: 10,
+            inStock: true,
+        },{
+            name: 'p2',
+            description: 'd2',
+            price: 20,
+            inStock: false,
+        }
+    ];
+    res.status(300).json(products);
+});
+
+app.post('/api/products',(req,res,next) => {
+    delete req.body._id;
+    res.status(201).json({msg: 'produit créé !'});
+});
+
 
 module.exports = app;
